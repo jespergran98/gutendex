1c7470e1f69a88a899eab1dc20b2a0e6605fbc2a
jespergran98 <jesper25sfj@gmail.com>
2025-12-19 10:22:03 +0100

    cleaned up filter and sort button stylings, added glass effect to search bar. Updated icons

--- src/components/SortButton/SortButton.jsx
+++ src/components/SortButton/SortButton.jsx
@@ -16,19 +16,31 @@ function SortButton() {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
+        width="24"
+        height="24"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
-        className={`sort-icon ${direction === 'up' ? 'rotate' : ''}`}
+        className="sort-icon"
       >
-        <path d="M3 6h18" />
-        <path d="M7 12h10" />
-        <path d="M10 18h4" />
+        {direction === 'down' ? (
+          <>
+            <path d="m3 16 4 4 4-4" />
+            <path d="M7 20V4" />
+            <path d="m21 8-4-4-4 4" />
+            <path d="M17 4v16" />
+          </>
+        ) : (
+          <>
+            <path d="m3 8 4-4 4 4" />
+            <path d="M7 4v16" />
+            <path d="m21 16-4 4-4-4" />
+            <path d="M17 20V4" />
+          </>
+        )}
       </svg>
       <span>Sort</span>
     </button>
